039488d456e8d96a77788e92a61642a4d0958145
Connor Cote <cotec4@gmail.com>
2020-11-01 23:17:10 -0500

    Updating routes and server.js

--- server.js
+++ server.js
@@ -1,71 +1,37 @@
-const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
-const path = require("path");
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 3030;
 
-const db = require("./models");
-
 const app = express();
 
+
+
+
 app.use(logger("dev"));
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workout", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-});
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/Workout',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
 
-// routes
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
-});
+  // routes
+app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/view.js"));
 
-app.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/exercise.html"));
-});
-
-app.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/stats.html"));
-});
-
-app.get("/api/workouts", (req, res) => {
-    db.Workout.find({})
-        .then((data) => res.json(data))
-        .catch((err) => res.status(500).send(err));
-});
-
-app.get("/api/workouts/range", (req, res) => {
-    db.Workout.find({}).limit(7)
-        .then(data => {
-            res.json(data);
-        })
-        .catch(err => {
-            res.json(err);
-        });
-});
-
-app.post("/api/workouts", (req, res) => {
-    const workout = req.body;
-    db.Workout.create(workout)
-        .then((data) => res.json(data));
-});
-
-app.put("/api/workouts/:id", (req, res) => {
-    const workout = req.body;
-    const id = req.params.id;
-    db.Workout.findByIdAndUpdate(id, { $push: { exercises: workout } })
-        .then(() => res.send(201))
-        .catch((err) => res.status(500).send(err));
-});
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
-});
+    console.log(`App running on port ${PORT}..`);
+})
